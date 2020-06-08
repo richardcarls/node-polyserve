75c82eea258c41f3f557b2cf8acc884272114b61
Rick Carls <rick@tpilb.net>
2020-06-08 11:20:44 -0400

    Add winston logger

---
+++ logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+const consoleTransport = new winston.transports.Console({
+  level: 'error',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+});
+
+const logger = winston.createLogger({
+  transports: [
+    consoleTransport,
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  consoleTransport.level = 'debug';
+}
+
+module.exports = logger;
