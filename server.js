883065ac266906d757a8233553562ac3637ba483
Rick Carls <rick@tpilb.net>
2020-06-08 09:17:47 -0400

    Add --port cli option

--- server.js
+++ server.js
@@ -10,13 +10,25 @@ const program = new Command();
 
 program
   .version(VERSION)
+  .option('-p, --port <number>', 'set server listen port. default is 80')
   .arguments('[root]')
-  .action(root => {
+  .action((root, options) => {
+    root = root || '.';
+    options.port = parseInt(options.port, 10) || '80';
+    
     const polyServe = new PolyServe();
 
-    http
-      .createServer(polyServe.getRequestHandler())
-      .listen(3000);
+    const server = http.createServer(polyServe.getRequestHandler());
+
+    server.on('close', (e) => {
+      console.info('Server closed, exiting.');
+
+      process.exit(0);
+    });
+    
+    server.listen(options.port, (e) => {
+      console.info(`PolyServe serving "${root}", listening on port ${options.port}.`);
+    });
   });
 
 program.parse(process.argv);
