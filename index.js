5108c52a6c4d4c5cdcae1121079f81477b8a9ea9
Rick Carls <rick@tpilb.net>
2020-06-09 11:20:09 -0400

    Resolve file paths and respond with file
    Assumes text/html and doens't support directory indices.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const VERSION = require('./package.json').version;
 const logger = require('./logger.js');
@@ -22,9 +23,55 @@ function polyserve(options) {
 
   function requestHandler(request, response) {
     logger.http(`HTTP ${request.method} ${request.url}`);
-    
-    response.statusCode = 418;
-    response.statusMessage = 'I\'m a teapot';
-    response.end();
+
+    // Resolve a file path from url and root
+    let fsPath;
+    try {
+      fsPath = path.normalize(path.join(root, request.url));
+    } catch(ex) {
+      response.statusCode = 404;
+      return response.end();
+    }
+
+    // Make sure we stay in server root
+    if (!fsPath.startsWith(root)) {
+      logger.warn('Request mapped to resource outside polyserve root', request);
+      
+      response.statusCode = 403;
+      return response.end();
+    }
+
+    // See if the file/directory exists
+    fs.stat(fsPath, (err, stat) => {
+      if (err) {
+        if (err.code === 'ENOENT' || err.code === 'ENOTDIR') {
+          // TODO: try index
+
+          response.statusCode = 404;
+          return response.end();
+        }
+
+        response.statusCode = 500;
+        return response.end();
+      }
+
+      // TODO: Serve directory index if stat is directory
+      if (stat.isDirectory()) {
+        response.statusCode = 404;
+        return response.end();
+      }
+      response.setHeader('content-length', stat.size);
+      // TODO: mime type discovery
+      response.setHeader('content-type', 'text/html');
+      
+      response.statusCode = 200;
+
+      fs.createReadStream(fsPath)
+        .on('error', err => {
+          response.statusCode = 500;
+          return response.end();
+        })
+        .pipe(response);
+    });
   }
 }
