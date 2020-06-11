5c613d88fa360544f9ae112bb65739dba960fdfa
Rick Carls <rick@tpilb.net>
2020-06-11 11:59:26 -0400

    Break out middleware functions, introduce ctx object

--- index.js
+++ index.js
@@ -18,58 +18,100 @@ class PolyServe {
   }
 
   requestHandler = (request, response) => {
-    logger.http(`HTTP ${request.method} ${request.url}`);
+    const ctx = {
+      app: this,
+      request,
+      response,
+    };
 
-    // Resolve a file path from url and root
-    let fsPath;
-    try {
-      fsPath = path.normalize(path.join(this._root, request.url));
-    } catch(ex) {
+    resolvePath(ctx);
+    router(ctx);
+    serveIndex(ctx);
+    serveFile(ctx);
+  }
+}
+
+function resolvePath(ctx) {
+  const { root } = ctx.app;
+  const { request, response } = ctx;
+
+  logger.http(`HTTP ${request.method} ${request.url}`);
+
+  let fsPath;
+  try {
+    fsPath = path.normalize(path.join(root, request.url));
+  } catch(ex) {
+    logger.error('Unable to resolve path', request);
+    
+    response.statusCode = 500;
+    return response.end();
+  }
+
+  // Make sure we stay in server root
+  if (!fsPath.startsWith(root)) {
+    logger.warn('Request mapped to resource outside polyserve root', request);
+    
+    response.statusCode = 403;
+    return response.end();
+  }
+
+  ctx.fsPath = fsPath;
+}
+
+function router(ctx) {
+  const { root } = ctx.app;
+  const { request, response, fsPath } = ctx;
+  
+  // See if the file/directory exists
+  let stat;
+  try {
+    stat = fs.statSync(fsPath);
+  } catch(ex) {
+    if (ex.code === 'ENOENT' || ex.code === 'ENOTDIR') {
+      // TODO: try index
+
       response.statusCode = 404;
       return response.end();
     }
 
-    // Make sure we stay in server root
-    if (!fsPath.startsWith(this._root)) {
-      logger.warn('Request mapped to resource outside polyserve root', request);
-      
-      response.statusCode = 403;
-      return response.end();
-    }
-
-    // See if the file/directory exists
-    fs.stat(fsPath, (err, stat) => {
-      if (err) {
-        if (err.code === 'ENOENT' || err.code === 'ENOTDIR') {
-          // TODO: try index
-
-          response.statusCode = 404;
-          return response.end();
-        }
-
-        response.statusCode = 500;
-        return response.end();
-      }
-
-      // TODO: Serve directory index if stat is directory
-      if (stat.isDirectory()) {
-        response.statusCode = 404;
-        return response.end();
-      }
-      response.setHeader('content-length', stat.size);
-      // TODO: mime type discovery
-      response.setHeader('content-type', 'text/html');
-      
-      response.statusCode = 200;
-
-      fs.createReadStream(fsPath)
-        .on('error', err => {
-          response.statusCode = 500;
-          return response.end();
-        })
-        .pipe(response);
-    });
+    response.statusCode = 500;
+    return response.end();
   }
+
+  ctx.stat = stat;
+}
+
+function serveIndex(ctx) {
+  const { root } = ctx.app;
+  const { request, response, fsPath, stat } = ctx;
+
+  if (!stat || !stat.isDirectory()) {
+    return;
+  }
+
+  response.statusCode = 404;
+  return response.end('unimplemented');
+}
+
+function serveFile(ctx) {
+  const { root } = ctx.app;
+  const { request, response, fsPath, stat } = ctx;
+
+  if (!stat || stat.isDirectory()) {
+    return;
+  }
+  
+  response.setHeader('content-length', stat.size);
+  response.setHeader('content-type', 'text/html');
+  
+  response.statusCode = 200;
+
+  fs.createReadStream(fsPath)
+    .on('error', err => {
+      response.statusCode = 500;
+      return response.end();
+    })
+    .pipe(response);
 }
 
 module.exports = exports = PolyServe;
